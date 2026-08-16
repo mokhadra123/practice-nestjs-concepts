490d13bd443a6d9735539508bd8a6382b1ff8bf5
Mohamed Khadra <m.khadra@penta-b.com>
2026-08-17 01:13:21 +0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,19 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  const swagger = new DocumentBuilder()
+    .setTitle('Nestjs Course - App Api')
+    .setVersion('1.0')
+    .addServer('http://localhost:5000')
+    .setLicense('khadra license', 'Ala ma tofrag')
+    .build();
+  const documentation = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api-docs', app, documentation);
 
   await app.listen(process.env.PORT ?? 5000);
 }
